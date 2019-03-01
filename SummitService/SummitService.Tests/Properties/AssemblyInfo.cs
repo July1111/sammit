21386ce81445df35f5e08e09a05e2e3c6fefa2c5
Nikita Partyko <npartyko@bk.ru>
2019-03-01 15:56:44 +0500

    Тест для AddCountry

---
+++ SummitService/SummitService.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SummitService.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SummitService.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c2105aea-fb13-4693-b7dd-85fe47603b55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SummitService/SummitService.Tests/Service1Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SummitService.Tests.ServiceReference1;
+
+namespace SummitService.Tests
+{
+    [TestClass]
+    public class Service1Tests
+    {
+        [TestMethod]
+        public void AddCountry_Russia_truereturned()
+        {
+            //arrange
+            string country = "Russia";
+            bool expected = true;
+            
+            //act
+            Service1Client client = new Service1Client("BasicHttpBinding_IService1");
+            var result = client.AddCountry(country);
+
+            //assert
+            Assert.AreEqual(expected, result.error);
+        }
+    }
+}
