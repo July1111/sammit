ee986b2e0913d6cd4a5364eef703d0fdb3cef7b6
julia2 <julia2@DESKTOP-15RMM78>
2019-03-11 15:16:41 +0500

    Формы пользователя

---
+++ Client/Client/Country.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для Country.xaml
+    /// </summary>
+    public partial class Country : Page
+    {
+        public Country()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Add.xaml", UriKind.Relative));
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Add.xaml", UriKind.Relative));
+        }
+    }
+}
